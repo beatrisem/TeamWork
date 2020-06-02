46032811fb1963d738b73be0b8bf280e5df5e780
sanita <sanita.berzina.g@gmail.com>
2020-06-02 20:25:45 +0300

    Added search button (functionality will be added tomorrow) Added more entries from the previously created db "Edit" doesn't work for me

--- src/main/java/com/example/demo/data/MySqlDataRepository.java
+++ src/main/java/com/example/demo/data/MySqlDataRepository.java
@@ -18,7 +18,7 @@ public class MySqlDataRepository implements RestDataRepository {
     private Connection connection;
     private static final String connectionString = "jdbc:mysql://localhost:3306/" + Constants.SCHEMA_NAME;
     private static final String connectionUser = "root";
-    private static final String connectionPassword = "utkkfxrfNEW21";
+    private static final String connectionPassword = "password";
 
     public MySqlDataRepository() {
         try {
